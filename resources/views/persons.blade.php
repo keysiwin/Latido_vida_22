2460ffcd11e16137a49a2eba617d2bda16118891
keysiwin <77917170@continental.edu.pe>
2024-06-27 03:50:33 -0500

    add3

--- resources/views/persons.blade.php
+++ resources/views/persons.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
     <div id="app">
         <persons-table></persons-table>
     </div>
-    @vite('resources/js/app.js')
 </body>
 </html>
+
+
