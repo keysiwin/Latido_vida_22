cca06ba561f9e1508cce44283eaeb2c3539dfcff
keysiwin <77917170@continental.edu.pe>
2024-06-27 11:05:39 -0500

    add14

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './resources/js'),
-            'vue': 'vue/dist/vue.esm-bundler.js',  // Alias para usar el compilador adecuado
+            'vue': 'vue/dist/vue.esm-bundler.js',
         },
     },
     build: {
@@ -27,5 +27,6 @@ export default defineConfig({
         },
     },
 });
+
 
 
